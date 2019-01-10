bae68f4a69a224b66ef164bd678908bc64442d52
Arnaud <arnaud33200@gmail.com>
2019-01-10 13:33:48 -0500

    Update RoomDatabaseUtil.java

--- Android/RoomDatabaseUtil.java
+++ Android/RoomDatabaseUtil.java
@@ -1,1 +1,10 @@
-
+public static void removeDatabaseFileAndReset() {
+    if (sInstance != null) { sInstance.close(); }
+    sInstance = null;
+    Context context = // Get Context here
+    String databaseName = // Get DatabaseName
+    File databaseFile = context.getDatabasePath(databaseName).getAbsoluteFile();
+    if (databaseFile.exists()) {
+        FileManager.deleteRecursive(databaseFile);
+    }
+}
